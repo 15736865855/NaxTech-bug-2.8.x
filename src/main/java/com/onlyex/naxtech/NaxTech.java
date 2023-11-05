1bf8cc82697f56ff133b209602ab3ad53c256ef0
OnlyEX <3107800043@qq.com>
2023-11-05 15:23:43 +0800

    0.1

--- src/main/java/com/onlyex/naxtech/NaxTech.java
+++ src/main/java/com/onlyex/naxtech/NaxTech.java
@@ -6,29 +6,38 @@ import com.onlyex.naxtech.common.block.NTMetaBlocks;
 import com.onlyex.naxtech.common.items.NTMetaInit;
 import com.onlyex.naxtech.common.metatileentities.NTMetaTileEntities;
 import com.onlyex.naxtech.proxy.CommonProxy;
+import gregtech.common.ConfigHolder;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import static com.onlyex.naxtech.api.NTValues.MOD_ID;
+
 @Mod(
-        modid = NTValues.MOD_ID,
+        modid = MOD_ID,
         name="NaxTech",
-        acceptedMinecraftVersions = "[1.12.2]",
+        acceptedMinecraftVersions = "[1.12.2,1.13)",
         version = "1.0" ,
         dependencies = "required-after:gregtech@[2.7.4-beta,) ;"
 )
 public class NaxTech {
-    @Mod.Instance(NTValues.MOD_ID)
+    @Mod.Instance(MOD_ID)
     public static NaxTech instance;
 
     @SidedProxy(
+            modId = MOD_ID,
             clientSide = "com.onlyex.naxtech.proxy.ClientProxy",
             serverSide = "com.onlyex.naxtech.proxy.CommonProxy"
     )
     public static CommonProxy proxy;
-
+    @Mod.EventHandler
+    public void onConstruction(FMLConstructionEvent event)
+    {
+        ConfigHolder.machines.highTierContent = true;
+    }
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
